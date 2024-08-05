60c5815950f2e4b8dfbedfdd629deaba814990b5
matthewd28 <matthewpalmerdavis@gmail.com>
2024-08-05 07:34:22 -0600

    Create variable grid size functionality

--- sketch.js
+++ sketch.js
@@ -1,16 +1,43 @@
 const container = document.querySelector("#container")
 
-for (let i = 0; i < 4; i++) {
-    const row = document.createElement("div")
-    row.setAttribute("id", "row")
-
-    for (let j = 0; j < 4; j++) {
-        const square = document.createElement("div")
-        square.setAttribute("id", "square")
-        square.textContent = `Square ${i + 1}`
-        row.appendChild(square)
-    }
-
+function createTiles(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div")
+        row.setAttribute("id", "row")
     
-    container.appendChild(row)
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div")
+            square.setAttribute("id", "square")
+            row.appendChild(square)
+        }
+    
+        
+        container.appendChild(row)
+    }
 }
+
+function removeTiles() {
+    const rows = container.querySelectorAll("#row")
+    rows.forEach((row) => {
+        const squares = row.querySelectorAll("square")
+        squares.forEach((square) => {
+            row.removeChild(square)
+        })
+        container.removeChild(row)
+    })
+}
+
+const generateButton = document.querySelector("#generate")
+const size = document.querySelector("#size")
+
+generateButton.addEventListener("click", () => {
+    if (size.value && size.value > 0 && size.value <= 100) {
+        console.log("entered")
+        removeTiles()
+        createTiles(size.value)
+    }
+    
+    
+})
+
+createTiles(4)
